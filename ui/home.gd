0503d374ad361d2cd0efbaffc3c7616349c2afb4
Sesso Kosga <kosgasesso@gmail.com>
2024-07-12 23:52:21 +0100

    feat (ui): Added a credit screen

---
+++ ui/home.gd
@@ -0,0 +1,31 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_play_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_upgrade_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_credits_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_back_pressed() -> void:
+	pass # Replace with function body.
